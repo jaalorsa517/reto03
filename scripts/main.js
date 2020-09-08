15ac5a390e862c56c449073602f0e60470ff2f5a
Jaime Ortiz <jaalorsa517@hotmail>
2020-09-07 22:09:43 -0500

    Finalizado el diseño responsivo

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,5 @@ import calculator from '../images/icon-calculator.svg'
 import karma from '../images/icon-karma.svg'
 import supervisor from '../images/icon-supervisor.svg'
 import teamBuilder from '../images/icon-team-builder.svg'
-const style = require('../styles/main.css');
+// const style = require('../styles/main.css');
+import style from '../styles/main.css'
